e9d8f7f3a68dd069f6112a7a11b349b06ed092cd
André P. Santos <andreztz@gmail.com>
2022-04-27 10:11:32 -0300

    Add greyscale filter

--- pset4/filter-less/helpers.c
+++ pset4/filter-less/helpers.c
@@ -1,8 +1,26 @@
 #include "helpers.h"
+#include <math.h>
+
 
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i=0; i < height; i++) 
+    {
+        for (int j=0; j < width; j++) 
+        {
+            BYTE avg = round(
+                (float)(
+                    image[i][j].rgbtRed +
+                    image[i][j].rgbtBlue +
+                    image[i][j].rgbtGreen
+                ) / 3
+            ); 
+            image[i][j].rgbtRed = avg;
+            image[i][j].rgbtBlue = avg;
+            image[i][j].rgbtGreen = avg;
+        }
+    }
     return;
 }
 
